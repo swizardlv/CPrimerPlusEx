f96aa97bee82099e960ac8630042041f6d751855
swizardlv <swizardlv@gmail.com>
2016-04-17 11:23:07 +0800

    add showf_pt.c

---
+++ charter3/showf_pt.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+int main(int argc, char const *argv[])
+{
+	float aboat=3200.0;
+	double abet=2.14e9;
+	long double dip=5.32e-5;
+	printf("%f can be written %e\n",aboat,aboat);
+	printf("%f can be written %e\n",abet,abet );
+	printf("%Lf can be written %Le\n",dip,dip );
+	return 0;
+}
